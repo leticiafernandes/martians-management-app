307948ba1d641c5d8318a8afe1b207bde732ef39
Leticia Fernandes <leticiacf.fernandes@gmail.com>
2021-04-04 23:58:33 -0300

    feat: encapsulate app into  global state management component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './stylesheets/index.scss'
 
+/** Components */
 import App from './components/App/index'
+import Store from './components/AppContext/store'
 
 import './stylesheets/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Store>
+      <App />
+    </Store>
   </React.StrictMode>,
   document.getElementById('root')
 )
